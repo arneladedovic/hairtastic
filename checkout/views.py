3f643133219291712d375bc434b8f805c2f037e1
arneladedovic <arneladedovic@hotmail.com>
2022-10-24 19:50:02 +0000

    Add stripeelements for payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LwVjqIEspgfEafdLVNPZIdDDvbg0tW4GdnTSmpdFEoWL6g5IhLPdsaBnBzkLvvav8ZEpDTK9HHeS77UBh6mvoYg00SeRraaRq',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
