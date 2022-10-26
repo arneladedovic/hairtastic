50de48bc7502c4509d4de2ca82e0d76f462f8ade
arneladedovic <arneladedovic@hotmail.com>
2022-10-26 04:17:39 +0000

    Add readme

--- hairtastic/settings.py
+++ hairtastic/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--rho)ql(xlacsitlx7g2@*8@tvoj!o@0#%f0o%qulq0!q(v%j^'
+os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
